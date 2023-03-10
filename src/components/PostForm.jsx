766ce2dbdd6beedfa9f90132b142498016283d94
Georgij Kravtsov <reasgicc@gmail.com>
2023-03-10 17:04:15 +0500

    Update PostForm.jsx

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -17,14 +17,12 @@ const PostForm = ({create}) => {
 
     return (
         <form>
-            {/*Управляемый компонент*/}
             <MyInput
                 value={post.title}
                 onChange={e => setPost({...post, title: e.target.value})}
                 type="text"
                 placeholder="Название поста"
             />
-            {/*Неуправляемый\Неконтролируемый компонент*/}
             <MyInput
                 value={post.body}
                 onChange={e => setPost({...post, body: e.target.value})}
