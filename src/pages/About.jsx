f3a94f4065c856143918f6d8926525d2e2a836a6
g1cc <reasgicc@gmail.com>
2023-03-10 16:15:37 +0500

    Signed-off-by: g1cc <reasgicc@gmail.com>

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Сайт создали Кравцов Георгий и Гарейнов Григорий
+        </h1>
+    );
+};
+
+export default About;
